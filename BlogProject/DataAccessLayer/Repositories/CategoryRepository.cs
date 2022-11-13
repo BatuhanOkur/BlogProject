2b97e9909daee5020087c881e0f54fc6d2c1f44f
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-13 10:58:26 +0300

    The method of creating instance from Context has been changed

--- BlogProject/DataAccessLayer/Repositories/CategoryRepository.cs
+++ BlogProject/DataAccessLayer/Repositories/CategoryRepository.cs
@@ -11,31 +11,36 @@ namespace DataAccessLayer.Repositories
 {
     public class CategoryRepository : ICategoryDal
     {
-        Context context = new Context();
+        
         public void AddCategory(Category category)
         {
+            using var context = new Context();
             context.Add(category);
             context.SaveChanges();
         }
 
         public void DeleteCategory(Category category)
         {
+            using var context = new Context();
             context.Remove(category);
             context.SaveChanges();
         }
 
         public Category GetCategory(int id)
         {
+            using var context = new Context();
             return context.Categories.Find(id);
         }
 
         public List<Category> ListAllCategories()
         {
+            using var context = new Context();
             return context.Categories.ToList();
         }
 
         public void UpdateCategory(Category category)
         {
+            using var context = new Context();
             context.Update(category); 
             context.SaveChanges();
         }
