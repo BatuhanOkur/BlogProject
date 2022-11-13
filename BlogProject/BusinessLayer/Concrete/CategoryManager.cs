39f85f4147f6e3518017736ad4bf0c43aea449ab
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-13 12:15:43 +0300

    CategoryManager created

--- BlogProject/BusinessLayer/Concrete/CategoryManager.cs
+++ BlogProject/BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,35 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
+        GenericRepository<Category> _repository = new GenericRepository<Category>();
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void CategoryDelete(Category category)
+        {
+            if(category != null) 
+            {
+                _repository.Delete(category);
+            }
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
