415737352072eda1e7a04159dca03572c3010966
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-13 21:00:28 +0300

    IBlog interface implemented in BlogManager

---
+++ BlogProject/BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogdal;
+
+		public BlogManager(IBlogDal blogdal)
+		{
+			_blogdal = blogdal;
+		}
+
+		public void BlogAdd(Blog blog)
+		{
+			_blogdal.Insert(blog);
+		}
+
+		public void BlogDelete(Blog blog)
+		{
+			_blogdal.Delete(blog);
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+			_blogdal.Update(blog);
+		}
+
+		public List<Blog> GetAllBlogs()
+		{
+			return _blogdal.GetAll();
+		}
+
+		public Blog GetBlog(int id)
+		{
+			return _blogdal.GetByID(id);
+		}
+	}
+}
