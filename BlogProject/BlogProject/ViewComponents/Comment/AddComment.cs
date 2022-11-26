7ed2e7b974bb275dc53c5bdf3154a2942f784f6e
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-26 15:14:11 +0300

    ViewComponents folder created

---
+++ BlogProject/BlogProject/ViewComponents/Comment/AddComment.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlogProject.ViewComponents.Comment
+{
+	public class AddComment : ViewComponent
+	{
+		CommentManager commentManager = new CommentManager(new EFCommentRepository());
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ BlogProject/BlogProject/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.Comment
+{
+	public class CommentListByBlog : ViewComponent
+	{
+		CommentManager commentManager = new CommentManager(new EFCommentRepository());
+		public IViewComponentResult Invoke()
+		{
+			var comments = commentManager.GetAllComments(3);
+			return View(comments);
+		}
+	}
+}
