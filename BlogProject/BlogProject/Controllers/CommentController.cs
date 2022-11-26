56c5fc7103a9051ff8a4bf3975ed7114a6ae0319
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-26 15:13:52 +0300

    CommentController modified

--- BlogProject/BlogProject/Controllers/CommentController.cs
+++ BlogProject/BlogProject/Controllers/CommentController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using BusinessLayer.Concrete;
 
 namespace BlogProject.Controllers
 {
 	public class CommentController : Controller
 	{
+		CommentManager commentManager = new CommentManager(new EFCommentRepository());
 		public IActionResult Index()
 		{
 			return View();
 		}
+
 	}
 }
