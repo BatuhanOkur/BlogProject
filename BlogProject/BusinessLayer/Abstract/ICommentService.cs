3473578bf9a130b6eb1bfa9b1b3d644eb09b9354
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-26 15:15:30 +0300

    CommentService has been added

---
+++ BlogProject/BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+	internal interface ICommentService
+	{
+		void CommentAdd(Comment Comment);
+		//void CommentDelete(Comment Comment);
+		//void CommentUpdate(Comment Comment);
+		List<Comment> GetAllComments(int id);
+		//Comment GetComment(int id);
+	}
+}
