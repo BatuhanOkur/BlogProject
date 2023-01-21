57b4c3c83dcc88c6955ef2516a3f79784e0ac43f
Batuhan Okur <batuhanokur2209@gmail.com>
2023-01-21 15:39:41 +0300

    Category datas has been listed in category list

---
+++ BlogProject/BlogProject/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.Category
+{
+	public class CategoryList : ViewComponent
+	{
+		CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = categoryManager.GetAllCategories();
+			return View(values);	
+		}
+	}
+}
