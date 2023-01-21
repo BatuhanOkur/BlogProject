4094bf7418e475e1dab746ec827983e6ab4aa409
Batuhan Okur <batuhanokur2209@gmail.com>
2023-01-21 15:38:44 +0300

    AuthorAdd added in authormanager

---
+++ BlogProject/BusinessLayer/Concrete/AuthorManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class AuthorManager : IAuthorService
+	{
+		IAuthorDal _authorDal;
+
+		public AuthorManager(IAuthorDal authorDal)
+		{
+			_authorDal = authorDal;
+		}
+		public void AuthorAdd(Author author)
+		{
+			_authorDal.Insert(author);
+		}
+	}
+}
