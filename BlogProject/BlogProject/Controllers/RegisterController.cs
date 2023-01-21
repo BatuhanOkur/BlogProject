a5f69d28ef251ec1fd19145d889bb92fa57b8816
Batuhan Okur <batuhanokur2209@gmail.com>
2023-01-21 15:40:49 +0300

    Register post and get methods have been created for registration

---
+++ BlogProject/BlogProject/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+	public class RegisterController : Controller
+	{
+		AuthorManager authorManager = new AuthorManager(new EFAuthorRepository());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Author author)
+		{
+			AuthorValidator authorValidator = new AuthorValidator();
+			ValidationResult results = authorValidator.Validate(author);
+			if(results.IsValid)
+			{
+				author.AuthorStatus = true;
+				author.AuthorAbout = "Kendinizden bahsedin!";
+				author.AuthorImage = "Boş";
+				authorManager.AuthorAdd(author);
+				return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach(var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+
+	}
+}
